bc9f9910134d6790fef137f1c525e814ef7415cd
ACTILAZION <chemkhien255@gmail.com>
2026-05-31 11:14:25 +0700

    Add reporting service project

---
+++ src/main/java/com/actilazion/ariesreportingproject/AriesReportingProjectApplication.java
@@ -0,0 +1,13 @@
+package com.actilazion.ariesreportingproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AriesReportingProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AriesReportingProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/actilazion/ariesreportingproject/AriesReportingProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.actilazion.ariesreportingproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AriesReportingProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
